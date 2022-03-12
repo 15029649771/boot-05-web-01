6b49f8bb6ab405cac6f8a8bca60b6cb87ef95d2c
Leo <leo@123.com>
2022-03-12 22:22:20 +0800

    Hot fix

--- src/main/java/com/bj/boot05web01/controller/HelloController.java
+++ src/main/java/com/bj/boot05web01/controller/HelloController.java
@@ -11,6 +11,7 @@ public class HelloController {
     public String hello(){
         System.out.println(666);
         System.out.println(777);
+        System.out.println(888);
         return "aaa";
     }
 }

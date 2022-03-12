930944b848a63b94485324698d9de9aa3c64bd73
Leo <leo@123.com>
2022-03-12 21:58:26 +0800

    IDEA

---
+++ src/main/java/com/bj/boot05web01/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.bj.boot05web01.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/111.jpg")
+    public String hello(){
+        return "aaa";
+    }
+}

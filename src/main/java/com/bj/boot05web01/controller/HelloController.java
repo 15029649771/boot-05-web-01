23ed379fa7f29d85e669f418f3047cea6c258da7
Leo <leo@123.com>
2022-03-12 22:01:44 +0800

    IDEA 2

--- src/main/java/com/bj/boot05web01/controller/HelloController.java
+++ src/main/java/com/bj/boot05web01/controller/HelloController.java
@@ -10,6 +10,7 @@ public class HelloController {
     @RequestMapping("/111.jpg")
     public String hello(){
         System.out.println(666);
+        System.out.println(777);
         return "aaa";
     }
 }
